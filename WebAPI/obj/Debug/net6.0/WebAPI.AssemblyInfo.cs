7230b26ef577c49bf03ad7362ce080fd17877e69
ayseelgoren <ayseelgoren@gmail.com>
2022-04-08 09:55:30 +0300

    update gitignore

--- WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1760fb18-981b-4ad4-8e20-cf499cb091ca")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
